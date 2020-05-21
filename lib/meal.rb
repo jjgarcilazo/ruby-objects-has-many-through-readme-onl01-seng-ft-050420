0db68c36f4aa2d5136562d22bb48cb6894b62e2c
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-21 06:08:09 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,9 +1,9 @@
 class Meal
-
+ 
   attr_accessor :waiter, :customer, :total, :tip
-
+ 
   @@all = []
-
+ 
   def initialize(waiter, customer, total, tip=0)
     @waiter = waiter
     @customer = customer
@@ -11,18 +11,7 @@ class Meal
     @tip = tip
     @@all << self
   end
-  jason = Waiter.new("Jason", 4)
-  lisa = Customer.new("Lisa", 24)
-  tim = Customer.new("Tim", 35)
-  terrance = Customer.new("Terrance", 27)
-
-  terrance.new_meal(jason, 50, 3)
-  lisa.new_meal(jason, 40, 10)
-  tim.new_meal(jason, 45, 8)
-
-  jason.best_tipper => <Customer:0x00007f80829959a8 @name="Lisa", @age=24>
-  jason.best_tipper.name => "Lisa"
-
+ 
   def self.all
     @@all
   end
