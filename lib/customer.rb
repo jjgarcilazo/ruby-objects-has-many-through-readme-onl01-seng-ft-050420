11a3eb2044332e59b3d7efc1cc10d6e3dc49e4ab
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-21 06:16:01 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -27,11 +27,11 @@ class Customer
 lisa = Customer.new("Lisa", 24)
 tim = Customer.new("Tim", 35)
 terrance = Customer.new("Terrance", 27)
- 
+
 terrance.new_meal(jason, 50, 3)
 lisa.new_meal(jason, 40, 10)
 tim.new_meal(jason, 45, 8)
- 
+
 jason.best_tipper => <Customer:0x00007f80829959a8 @name="Lisa", @age=24>
 jason.best_tipper.name => "Lisa"
 end
