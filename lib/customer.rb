1bd91771fb2ce5d8af470e2bd7576665868c1a2a
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-21 06:15:57 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -23,15 +23,15 @@ class Customer
     end
   end
 
-  terrance = Customer.new("Terrance", 27)
-jason = Waiter.new("Jason", 4)
-andrew = Waiter.new("Andrew", 7)
-yomi = Waiter.new("Yomi", 10)
-
-terrance.new_meal(jason, 50, 6)
-terrance.new_meal(andrew, 60, 8)
-terrance.new_meal(yomi, 30, 4)
-
-terrance.waiters => [<Waiter:0x00007fa23f18f860 @name="Jason", @yrs_experience=34>, <Waiter:0x00007fa23f196818 @name="Andrew", @yrs_experience=27>, <Waiter:0x00007fa23f19dd20 @name="Yomi", @yrs_experience=20>]
-terrance.waiters.length => 3
+  jason = Waiter.new("Jason", 4)
+lisa = Customer.new("Lisa", 24)
+tim = Customer.new("Tim", 35)
+terrance = Customer.new("Terrance", 27)
+ 
+terrance.new_meal(jason, 50, 3)
+lisa.new_meal(jason, 40, 10)
+tim.new_meal(jason, 45, 8)
+ 
+jason.best_tipper => <Customer:0x00007f80829959a8 @name="Lisa", @age=24>
+jason.best_tipper.name => "Lisa"
 end
